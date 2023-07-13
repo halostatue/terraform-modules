e3ecd80b612ce16c8c71839a2bc9d7d390a5ce2f
Austin Ziegler <austin@zieglers.ca>
2023-07-12 23:20:56 -0400

    Allow terraform-provider-aws v5.x

--- aws/acm-certificate/versions.tf
+++ aws/acm-certificate/versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = ">= 4.10.0, < 6.0.0"
 
       configuration_aliases = [aws.cloudfront-certificates, aws.route53]
     }
